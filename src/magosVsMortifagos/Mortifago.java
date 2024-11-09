1366b3a7fcf68c3467a1c55e85f6747c418eb994
tomascrotti <tomascrotti02@gmail.com>
2024-11-09 15:34:49 -0300

    Nueva clase Mortifago

--- src/magosVsMortifagos/Mortifago.java
+++ src/magosVsMortifagos/Mortifago.java
@@ -1,14 +1,14 @@
 package magosVsMortifagos;
 
-public class Mago extends Personaje {
-	public Mago(String nombre, int nivelMagia, double puntosVida, Varita varita) {
+public class Mortifago extends Personaje {
+
+	public Mortifago(String nombre, int nivelMagia, double puntosVida, Varita varita) {
 		super(nombre, nivelMagia, puntosVida, varita);
-		System.out.println("out");
 	}
 
 	@Override
 	public void lanzarHechizo(Hechizo hechizo, Personaje objetivo) {
-				
+		
 	}
 	
 }
