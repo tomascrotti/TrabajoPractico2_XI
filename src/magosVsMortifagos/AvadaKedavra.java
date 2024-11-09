3eedaf190f467d2030d5f18356b0470556852229
Joel M <59495685+naGiX1212@users.noreply.github.com>
2024-11-09 15:44:32 -0300

    avadakabraa

--- src/magosVsMortifagos/AvadaKedavra.java
+++ src/magosVsMortifagos/AvadaKedavra.java
@@ -3,7 +3,8 @@ package magosVsMortifagos;
 public class AvadaKedavra implements Hechizo {
 	@Override
 	public void ejecutar(Personaje lanzador, Personaje objetivo) {
-		// TODO Auto-generated method stub
-		
+		int danio = 50 ;
+		objetivo.recibirDmg(50);
+		System.out.println(lanzador.nombre + " ha lanzado Avada Kedavra a " + objetivo.nombre + " y le ha quitado 50 puntos de vida.");
 	}
 }
