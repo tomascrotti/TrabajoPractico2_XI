ade91bcbd3ae201f6add76de1d950dcae1e8ef93
Joel M <59495685+naGiX1212@users.noreply.github.com>
2024-11-09 14:23:39 -0300

    clase creada

---
+++ src/magosVsMortifagos/PersonajeFactory.java
@@ -0,0 +1,15 @@
+package magosVsMortifagos;
+
+public class PersonajeFactory {
+
+    public static Personaje crearMago() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearMago'");
+    }
+
+    public static Personaje crearMortifago() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearMortifago'");
+    }
+
+}
