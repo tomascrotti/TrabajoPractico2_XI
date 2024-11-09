7db03e07405b683ef2750f245d4d7cbc2fb46ead
Joel M <59495685+naGiX1212@users.noreply.github.com>
2024-11-09 14:16:43 -0300

    agregado Seguidor

---
+++ src/magosVsMortifagos/Seguidor.java
@@ -0,0 +1,6 @@
+package magosVsMortifagos;
+public class Seguidor extends Mago{
+    public Seguidor(String nombre,int nivelMagia,Double puntosVida){
+        super(nombre,nivelMagia,puntosVida,Varita.EXPERTO);
+    }
+}
