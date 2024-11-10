fdf1d7b16f9cf9868c7bc82fae8e90a978a419c2
Juan Martin Coronato <jmcoronato@gmail.com>
2024-11-10 13:26:45 -0300

    prolog

--- src/magosVsMortifagos/Main.java
+++ src/magosVsMortifagos/Main.java
@@ -1,31 +1,41 @@
 package magosVsMortifagos;
 
+import org.jpl7.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("       ███    ███  █████   ██████   ██████  ███████     ██    ██ ███████\r\n"
-				         + "       ████  ████ ██   ██ ██       ██    ██ ██          ██    ██ ██     \r\n"
-				         + "       ██ ████ ██ ███████ ██   ███ ██    ██ ███████     ██    ██ ███████\r\n"
-          				 + "       ██  ██  ██ ██   ██ ██    ██ ██    ██      ██      ██  ██       ██\r\n"
-				         + "       ██      ██ ██   ██  ██████   ██████  ███████       ████   ███████\r\n"
-				         + "                                                                 \r\n"
-				         + "███    ███  ██████  ██████  ████████ ██ ███████  █████   ██████   ██████  ███████\r\n"
-				         + "████  ████ ██    ██ ██   ██    ██    ██ ██      ██   ██ ██       ██    ██ ██     \r\n"
-				         + "██ ████ ██ ██    ██ ██████     ██    ██ █████   ███████ ██   ███ ██    ██ ███████\r\n"
-				         + "██  ██  ██ ██    ██ ██   ██    ██    ██ ██      ██   ██ ██    ██ ██    ██      ██\r\n"
-				         + "██      ██  ██████  ██   ██    ██    ██ ██      ██   ██  ██████   ██████  ███████\r\n"
-				         + "                                                                                     ");
-		BatallaMagosVsMortifagos batalla = new BatallaMagosVsMortifagos();
-		Batallon batallonMagos = new Batallon();
-		Batallon batallonMortifagos = new Batallon();
+//		System.out.println("       ███    ███  █████   ██████   ██████  ███████     ██    ██ ███████\r\n"
+//				         + "       ████  ████ ██   ██ ██       ██    ██ ██          ██    ██ ██     \r\n"
+//				         + "       ██ ████ ██ ███████ ██   ███ ██    ██ ███████     ██    ██ ███████\r\n"
+//          				 + "       ██  ██  ██ ██   ██ ██    ██ ██    ██      ██      ██  ██       ██\r\n"
+//				         + "       ██      ██ ██   ██  ██████   ██████  ███████       ████   ███████\r\n"
+//				         + "                                                                 \r\n"
+//				         + "███    ███  ██████  ██████  ████████ ██ ███████  █████   ██████   ██████  ███████\r\n"
+//				         + "████  ████ ██    ██ ██   ██    ██    ██ ██      ██   ██ ██       ██    ██ ██     \r\n"
+//				         + "██ ████ ██ ██    ██ ██████     ██    ██ █████   ███████ ██   ███ ██    ██ ███████\r\n"
+//				         + "██  ██  ██ ██    ██ ██   ██    ██    ██ ██      ██   ██ ██    ██ ██    ██      ██\r\n"
+//				         + "██      ██  ██████  ██   ██    ██    ██ ██      ██   ██  ██████   ██████  ███████\r\n"
+//				         + "                                                                                     ");
+//		BatallaMagosVsMortifagos batalla = new BatallaMagosVsMortifagos();
+//		Batallon batallonMagos = new Batallon();
+//		Batallon batallonMortifagos = new Batallon();
+//		
+//		
+//
+//		for (int i = 0; i < 3; i++) {
+//			batallonMagos.agregarPersonaje(PersonajeFactory.crearMago());
+//			batallonMortifagos.agregarPersonaje(PersonajeFactory.crearMortifago());
+//		}
+//		batalla.iniciarBatalla(batallonMagos,batallonMortifagos);
 		
-		
+		// Cargar el archivo Prolog
+        Query q1 = new Query("consult", new Term[] {new Atom("test.pl")});
+        System.out.println("consult('test.pl') " + (q1.hasSolution() ? "succeeded" : "failed"));
 
-		for (int i = 0; i < 3; i++) {
-			batallonMagos.agregarPersonaje(PersonajeFactory.crearMago());
-			batallonMortifagos.agregarPersonaje(PersonajeFactory.crearMortifago());
-		}
-		batalla.iniciarBatalla(batallonMagos,batallonMortifagos);
+        // Realizar la consulta
+        Query q2 = new Query("child_of", new Term[] {new Atom("joe"), new Atom("ralf")});
+        System.out.println("child_of(joe,ralf) is " + (q2.hasSolution() ? "provable" : "not provable"));
 	}
 }
